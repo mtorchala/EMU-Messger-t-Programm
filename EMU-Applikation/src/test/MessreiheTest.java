92393f7d02aea737a7ba8a49bb953c69e788d3ac
mtorchala <marc.torchala@hs-bochum.de>
2018-06-19 22:48:56 +0200

    TestNG weiterimplementiert mit Annotationen!

--- EMU-Applikation/src/test/MessreiheTest.java
+++ EMU-Applikation/src/test/MessreiheTest.java
@@ -7,48 +7,47 @@ import business.Messreihe;
 
 public class MessreiheTest {
 	
-	
+	Messreihe messreihe;
 	
 	@Test
 	public void konstruktorTest(){
-		Messreihe messreihe = new Messreihe(1,20,"LED","Leistung");
+		messreihe = new Messreihe(1,20,"LED","Leistung");
 		Assert.assertTrue(1 == messreihe.getMessreihenId());
 		Assert.assertTrue(20 == messreihe.getZeitintervall());
 		Assert.assertTrue(messreihe.getVerbraucher().equals("LED"));
 		Assert.assertTrue(messreihe.getMessgroesse().equals("Leistung"));
 	}
 	
-	@Test
+	@Test(expectedExceptions = {IllegalArgumentException.class})
 	public void Mkl0_Z15_Snull_MaMl(){
-		Messreihe messreihe = new Messreihe(-1,20,null,"Arbeit");
+		messreihe = new Messreihe(-1,20,null,"Arbeit");
 	}
 	
-	@Test
+	@Test(expectedExceptions = {IllegalArgumentException.class})
 	public void Mkl0_Z15_Sleer_MaMl(){
-		Messreihe messreihe = new Messreihe(-1,20,"","Leistung");
+		messreihe = new Messreihe(-1,20,"","Leistung");
 		
 	}
 	
-	@Test
+	@Test(expectedExceptions = {IllegalArgumentException.class})
 	public void Mkl0_Z15_Sverbraucher_MaMl(){
-		Messreihe messreihe = new Messreihe(-1,20,"LED","Arbeit");
+		messreihe = new Messreihe(-1,20,"LED","Arbeit");
 		
 	}
 	
-	@Test
+	@Test(expectedExceptions = {IllegalArgumentException.class})
 	public void Mgrgl0_Z15_Snull_MaMl(){
-		Messreihe messreihe = new Messreihe(1,20,null,"Leistung");
+		messreihe = new Messreihe(1,20,null,"Leistung");
 		
 	}
-	@Test
+	@Test(expectedExceptions = {IllegalArgumentException.class})
 	public void Mgrgl0_Z15_Sleer_MaMl(){
-		Messreihe messreihe = new Messreihe(1,20,"","Arbeit");
+		messreihe = new Messreihe(1,20,"","Arbeit");
 		
 	}
 	
-	@Test
 	public void Mgrgl0_Z15_Sverbraucher_MaMl(){
-		Messreihe messreihe = new Messreihe(1,20,"LED","Leistung");
+		messreihe = new Messreihe(1,20,"LED","Leistung");
 		
 	}
 }
